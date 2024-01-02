c217caec0820d77c34664ace60fab5f8d90eae21
Ahsan-Bashir <bteeccc@gmail.com>
2024-01-02 16:32:02 +0500

    configured home page

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express=require('express')
-const app=express()
 const path=require('path')
+const expressLayouts = require('express-ejs-layouts');
+const app=express()
 const port = process.env.PORT || 5000
+
+//To pass data through forms
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(methodOverride('_method'))
 
 //Templating Engine
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
+
+// Use the layout middleware
+app.use(expressLayouts);
+app.set("layout", "index");
+
 //static files
 app.use('/assets',express.static('assets'))
 
